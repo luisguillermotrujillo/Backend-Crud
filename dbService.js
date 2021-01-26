bbbd525e773ad485cb8933a28a3c4e0c066ff23c
luis gullermo <luisguillermotrujillo16@gmail.com>
2021-01-25 21:10:22 -0500

    creando enlace con la base de datos

--- dbService.js
+++ dbService.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 const dotenv = require("dotenv");
-dotenv.config();
+let instance = null;
+const result = dotenv.config();
+
+if (result.err) {
+    throw result.err
+}
+console.log(result.parsed);
+console.log(process.env.USER);
 
 const connection = mysql.createConnection({
     host:process.env.HOST,
-    user:process.env.USERNAME,
+    user:process.env.USER,
     password:process.env.PASSWORD,
     database:process.env.DATABASE,
     port:process.env.DB_PORT
@@ -15,4 +22,30 @@ connection.connect((err)=>{
         console.log(err.message);
     }
     console.log("db" + connection.state);
-})
+});
+
+class DbService{
+    static getDbServiceInstance(){
+        return instance ? instance: new DbService();
+    }
+
+    async getAllData(){
+        try{
+            const response = await new Promise((
+                resolve,reject
+            ) =>{
+                const query = "SELECT * FROM nombres";
+                connection.query(query,(err,result) =>{
+                    if (err) {reject(new Error(err.message));
+                        
+                    }
+                })
+            })
+
+        }
+        catch (error){
+            console.log(error);
+        }
+    }
+}
+module.exports = DbService;
